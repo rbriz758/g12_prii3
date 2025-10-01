7ac4d8874b031e3de7d559b066cc7b951350a9d5
Tu Nombre <rbriram@upv.edu.es>
2025-10-01 17:34:59 +0200

    Launcher añadido

--- g12_prii3_ws/src/g12_prii3_turtlesim/launch/mover_tortuga.launch.py
+++ g12_prii3_ws/src/g12_prii3_turtlesim/launch/mover_tortuga.launch.py
@@ -3,14 +3,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Nodo del simulador turtlesim
         Node(
             package='turtlesim',
             executable='turtlesim_node',
-            name='sim'
+            name='simulador_tortuga'
         ),
+        # Nodo personalizado para mover la tortuga
         Node(
             package='g12_prii3_turtlesim',
             executable='mover_tortuga',
-            name='controller'
+            name='mover_tortuga'
         )
     ])
