4ac3e0503007279f7a54ad6ec6738b3246b5550c
Tu Nombre <rbriram@upv.edu.es>
2025-10-06 12:56:48 +0200

    Arreglo en setup.py

--- g12_prii3_ws/src/g12_prii3_turtlesim/setup.py
+++ g12_prii3_ws/src/g12_prii3_turtlesim/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'g12_prii3_turtlesim'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # AÑADE ESTAS LÍNEAS PARA INCLUIR LOS ARCHIVOS LAUNCH
+        (os.path.join('share', package_name, 'launch'), 
+         glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
